4b98f9e1321a67a7879c029aeb82d74d5cb0f575
RHNCG21 <140030302+RHNCG21@users.noreply.github.com>
2024-09-12 21:29:55 +0000

    adding env

--- api/api.py
+++ api/api.py
@@ -1,21 +1,26 @@
 import json
 from flask import Flask, jsonify, request
+import os
 
 app = Flask(__name__)
-
+token = os.getenv('TOKEN')
 # Initialize global_data outside the function
 global_data = {"hello": "world"}
 
 @app.route('/data', methods=['GET', 'POST'])
 def get_data():
-    if request.method == 'POST':
-        data = request.get_json()
-        # Append new data to the global_data dictionary
-        global global_data
-        global_data.update(data)
-        return jsonify({"message": "data appended successfully"})
+    auth_key = request.headers.get('Authorization')
+    if auth_key == token:
+        if request.method == 'POST':
+            data = request.get_json()
+            # Append new data to the global_data dictionary
+            global global_data
+            global_data.update(data)
+            return jsonify({"message": "data appended successfully"})
+        else:
+            return jsonify(global_data)  # Return the current global_data
     else:
-        return jsonify(global_data)  # Return the current global_data
+        return jsonify({"error": "Unauthorized"}), 401
 
 if __name__ == '__main__':
     app.run()
